7c7a9b27176491817807aff5609fb62303272fc9
lecfab <lecfab@gmail.com>
2017-04-12 05:13:27 +0200

    rerandomization and bestfreq implementation

--- dicotomix.py
+++ dicotomix.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import random
 import bisect
+from math import log
 
 class Direction(Enum):
     LEFT = -1
@@ -24,7 +25,8 @@ class Dicotomix:
         self._words = words # words is a (cumulative frequency, word) list
         self._words.insert(0, (0., self._words[0][1])) # add a dummy word with frequency 0. for symmetry
         self._stack = []
-        self._EPSILON = 0
+        self._EPSILON = 1/1000 # size of random interval
+        self._EPSILON2 = 1/10 # size of interval where best freq is searched
         self._letters = letters
         if self._letters != None:
             self._letters.insert(0, (0., self._letters[0][1]))
@@ -38,9 +40,42 @@ class Dicotomix:
 
     def _push(self, left, right):
         mid = (self._words[left][0] + self._words[right][0]) / 2.0
-        randomization = random.uniform(-1, 1) * self._EPSILON
-        frequency = mid + (self._words[right][0] - self._words[left][0]) * randomization
-        cursor = self._findWordIndexFromFrequency(frequency)
+        intervalSize = self._words[right][0] - self._words[left][0]
+        if True: # True to use random interval
+            randomization = random.uniform(-1, 1) * self._EPSILON
+            frequency = mid + intervalSize * randomization
+            cursor = self._findWordIndexFromFrequency(frequency)
+        else: # to activate highest frequency in interval
+            boundLeft = self._findWordIndexFromFrequency(mid - intervalSize * self._EPSILON2/2)
+            boundRight = self._findWordIndexFromFrequency(mid + intervalSize * self._EPSILON2/2)
+            mobileCursor = boundLeft
+            cursor = mobileCursor
+            bestFreq = 0
+            while mobileCursor <= boundRight:
+                tmpFreq = self._words[mobileCursor][0]-self._words[mobileCursor-1][0]
+                if tmpFreq > bestFreq:
+                    bestFreq = tmpFreq
+                    cursor = mobileCursor
+                mobileCursor+=1
+        """else: # to activate highest frequency in interval
+            boundLeft = self._findWordIndexFromFrequency(mid - intervalSize * self._EPSILON2/2)
+            boundRight = self._findWordIndexFromFrequency(mid + intervalSize * self._EPSILON2/2)
+            mobileCursor = boundLeft
+            cursor = mobileCursor
+            bestEntropy = 0
+            while mobileCursor <= boundRight:
+                freqBefore = self._words[mobileCursor-1][0]
+                freqAfter = 1 - self._words[mobileCursor][0]
+                freqWord = 1 - freqAfter - freqBefore
+                tmpEntropy = freqWord*log(freqWord, 3)
+                if freqBefore > 0:
+                    tmpEntropy += freqBefore*log(freqBefore, 3)
+                if freqAfter > 0:
+                    tmpEntropy += freqAfter*log(freqAfter, 3)
+                if tmpEntropy < bestEntropy:
+                    bestEntropy = tmpEntropy
+                    cursor = mobileCursor
+                mobileCursor+=1 # """
         self._stack.append(_State(left, cursor, right))
 
         return (self._words[left][1], self._words[cursor][1], self._words[right][1])
